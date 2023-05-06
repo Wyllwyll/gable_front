e0216ca1586f28961e96593efcb6ebdb32df856f
TB <terry.boeuf@hotmail.fr>
2023-05-06 14:45:39 +0200

    fix:oublie de commentaire

--- src/context/SelectionContext.tsx
+++ src/context/SelectionContext.tsx
@@ -4,14 +4,14 @@ import { TOrders } from "../PanelUser/tipage/TOrders";
 
 
 
-
+//définit les types pour les objets selections et order, ainsi que les fonctions setSelections et setOrder qui modifient ces objets.
 interface SelectionContextType {
     selections: { [key: string]: Tcomposants | undefined };
     setSelections: (value: { [key: string]: Tcomposants | undefined }) => void;
     order: TOrders | null;
     setOrder: (value: TOrders | null) => void;
 }
-
+// ajoute une propriété value qui peut être utilisée pour définir des valeurs initiales pour le contexte.
 interface SelectionProviderProps extends PropsWithChildren<{}> {
     value?: SelectionContextType;
 }
@@ -22,7 +22,7 @@ export const SelectionContext = React.createContext<SelectionContextType>({
     order: null,
     setOrder: () => { },
 });
-
+// fournit les valeurs de selections et order à partir de l'objet value fourni ou d'un objet vide s'il n'est pas fourni. Il renvoie également le Provider du contexte avec les valeurs fournies.
 export const SelectionProvider: FC<SelectionProviderProps> = ({ children, value }) => {
     const [selections, setSelections] = useState<{ [key: string]: Tcomposants | undefined }>(
         value?.selections ?? {}
--- src/context/UserContext.ts
+++ src/context/UserContext.ts
@@ -5,5 +5,5 @@ import { DEFAULT_USER } from "../constant/TVisitor";
 
 export const UserContext = React.createContext({
     user: DEFAULT_USER,
-    setUser: (value: TUser) => { },
+    setUser: (value: TUser) => { }, //prend en paramètre un objet de type TUser et qui ne retourne rien. Cette fonction est utilisée pour mettre à jour les informations de l'utilisateur dans le contexte UserContext.
 });
